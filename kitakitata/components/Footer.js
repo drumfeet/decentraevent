2ebcb5a67b69b7518f2795377c0f912f74fea78a
drumfeet <drumfeet@gmail.com>
2023-04-25 14:44:59 +0800

    wip

--- kitakitata/components/Footer.js
+++ kitakitata/components/Footer.js
@@ -1,85 +1,101 @@
 import {
   Box,
-  chakra,
+  Button,
   Container,
+  Input,
   Link,
   Stack,
   Text,
-  useColorModeValue,
-  VisuallyHidden,
 } from "@chakra-ui/react"
 import { useContext } from "react"
-import { FaLink, FaGithub } from "react-icons/fa"
 import { AppContext } from "@/context/AppContext"
-
-const Logo = (props) => {
-  return (
-    <>
-      <Link href="https://weavedb.dev" isExternal>
-        <Text fontSize="sm">Built with WeaveDB ♥️</Text>
-      </Link>
-    </>
-  )
-}
-
-const SocialButton = ({ children, label, href }) => {
-  const buttonProps = {
-    bg: useColorModeValue("blackAlpha.100", "whiteAlpha.100"),
-    rounded: "full",
-    w: 8,
-    h: 8,
-    cursor: "pointer",
-    display: "inline-flex",
-    alignItems: "center",
-    justifyContent: "center",
-    transition: "background 0.3s ease",
-    _hover: {
-      bg: useColorModeValue("blackAlpha.200", "whiteAlpha.200"),
-    },
-  }
-
-  return (
-    <chakra.button
-      as={"a"}
-      href={href}
-      target={"_blank"}
-      rel={"noopener noreferrer"}
-      {...buttonProps}
-    >
-      <VisuallyHidden>{label}</VisuallyHidden>
-      {children}
-    </chakra.button>
-  )
-}
+import { toast } from "react-toastify"
 
 export default function Footer() {
   const { contractTxId } = useContext(AppContext)
 
   return (
-    <Box marginTop={28} bg="brand.deepPurple" color="whiteAlpha.900">
-      <Container
-        as={Stack}
-        maxW={"6xl"}
-        py="48px"
-        direction={{ base: "column", md: "row" }}
-        spacing={4}
-        justify={{ base: "center", md: "space-between" }}
-        align={{ base: "center", md: "center" }}
+    <>
+      <Stack position="relative">
+        <Box mt={28} bg="brand.deepPurple">
+          <Container height="200px" />
+        </Box>
+
+        <Box position="relative" top="-100px">
+          <Container
+            bg="white"
+            borderRadius="16px"
+            boxShadow="xl"
+            p="64px"
+            display="flex"
+            as={Stack}
+            maxW={"6xl"}
+            direction={{ base: "column", md: "row" }}
+            spacing={4}
+            justify={{ base: "center", md: "space-between" }}
+            align={{ base: "center", md: "center" }}
+          >
+            <Box flex="1">
+              <Text fontSize="4xl" fontWeight="bold">
+                Join our newsletter
+              </Text>
+              <Text fontSize="lg">Signup for the very exciting meetups.</Text>
+            </Box>
+            <Box flex="1">
+              <Stack direction={{ base: "column", md: "row" }} spacing={"12px"}>
+                <Input
+                  w="100%"
+                  variant={"solid"}
+                  borderWidth={1}
+                  id={"email"}
+                  type={"email"}
+                  required
+                  placeholder={"Your Email"}
+                  aria-label={"Your Email"}
+                />
+                <Button
+                  py="12px"
+                  px="20px"
+                  onClick={() => toast("Feature coming soon!")}
+                >
+                  Subscribe
+                </Button>
+              </Stack>
+            </Box>
+          </Container>
+        </Box>
+      </Stack>
+
+      <Box
+        bg="brand.deepPurple"
+        color="whiteAlpha.900"
+        id="footerMain"
+        position="relative"
       >
-        <Text>Kitakitata</Text>
-        <Stack direction={"row"} spacing={6}>
-          <Text>
-            Powered by{" "}
-            <Link
-              href="https://weavedb.dev"
-              isExternal
-              textDecoration="underline"
-            >
-              WeaveDB
-            </Link>
-          </Text>
-        </Stack>
-      </Container>
-    </Box>
+        <Container
+          as={Stack}
+          maxW={"6xl"}
+          py="48px"
+          direction={{ base: "column", md: "row" }}
+          spacing={4}
+          justify={{ base: "center", md: "space-between" }}
+          align={{ base: "center", md: "center" }}
+        >
+          <Text fontWeight="bold">Kitakitata</Text>
+          <Stack direction={"row"} spacing={6}>
+            <Text>
+              Powered by{" "}
+              <Link
+                href="https://weavedb.dev"
+                isExternal
+                textDecoration="underline"
+              >
+                WeaveDB
+              </Link>
+            </Text>
+          </Stack>
+        </Container>
+      </Box>
+    </>
   )
 }
