4bca950d17c04bbf429d78ccfdb9d0b2ff5e3494
drumfeet <drumfeet@gmail.com>
2023-05-09 21:20:56 +0800

    wip

--- kitakitata/pages/view-attendees/[eventId].js
+++ kitakitata/pages/view-attendees/[eventId].js
@@ -1,6 +1,5 @@
 import Layout from "@/components/Layout"
 import {
-  Flex,
   Button,
   TableContainer,
   Table,
@@ -15,20 +14,27 @@ import {
   HStack,
   Stack,
   Text,
+  Divider,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Spacer,
 } from "@chakra-ui/react"
 import { useContext, useEffect, useState } from "react"
 import { AppContext } from "@/context/AppContext"
 import { useRouter } from "next/router"
-import { isNil } from "ramda"
 import { toast } from "react-toastify"
-import { CalendarIcon, DownloadIcon, TimeIcon } from "@chakra-ui/icons"
-import GoBack from "@/components/GoBack"
+import {
+  CalendarIcon,
+  DownloadIcon,
+  Search2Icon,
+  TimeIcon,
+} from "@chakra-ui/icons"
 import { GoLocation } from "react-icons/go"
-import queryString from "query-string"
 
 export default function ViewAttendees() {
   const router = useRouter()
-  const { eventId, data } = router.query
+  const { eventId } = router.query
   const { initDB, getEventAttendees, getEventWithEventId } =
     useContext(AppContext)
   const [eventAttendees, setEventAttendees] = useState([])
@@ -76,16 +82,6 @@ export default function ViewAttendees() {
   return (
     <Layout>
       <Container maxW={"8xl"}>
-        <Box justifyContent="flex-end" my="28px">
-          <Button
-            onClick={handleDownload}
-            rightIcon={<DownloadIcon />}
-            alignSelf="flex-end"
-          >
-            Download Attendees
-          </Button>
-        </Box>
-
         <Box
           h="291px"
           bgGradient="linear-gradient(90deg, #A163B9 0%, #874DA1 14.06%, #593980 27.2%, #413A78 40.39%, #3D5584 52.48%, #426F93 64.13%, #518BA4 74.25%, #5EA6B5 83.04%, #5FAFBB 90.95%, #67B5BC 97.99%)"
@@ -111,44 +107,81 @@ export default function ViewAttendees() {
 
         <Stack spacing="8px">
           <HStack>
-            <Heading>Event Name</Heading>
+            <Heading fontSize="28px" fontWeight="500">
+              {eventData?.data?.title}
+            </Heading>
             <HStack flex="1" ml="auto" justifyContent="flex-end">
               <CalendarIcon />
               <Text>{eventData?.data?.start_time}</Text>
               <TimeIcon />
-              <Text>Time</Text>
+              <Text>{eventData?.data?.start_time}</Text>
             </HStack>
           </HStack>
           <HStack justifyContent="flex-end">
             <GoLocation />
-            <Text>Location</Text>
+            <Text>{eventData?.data?.location}</Text>
           </HStack>
+
+          <Stack>
+            <Box p="24px">
+              <Text fontSize="18px" fontWeight="500">
+                Attendees
+              </Text>
+            </Box>
+            <Divider />
+
+            <Stack
+              spacing={{ base: 4, md: 0 }}
+              direction={{ base: "column", md: "row" }}
+            >
+              <InputGroup hidden={true}>
+                <InputLeftElement
+                  pointerEvents="none"
+                  children={<Search2Icon color="black.border" />}
+                />
+                <Input
+                  placeholder="Search for attendees"
+                  borderColor="black.border"
+                  borderRadius={0}
+                  w={{ base: "100%", md: "400px" }}
+                />
+              </InputGroup>
+              <Spacer />
+              <Button
+                onClick={handleDownload}
+                rightIcon={<DownloadIcon />}
+                py="14px"
+                px="58px"
+                ml="auto"
+              >
+                Download Attendees
+              </Button>
+            </Stack>
+
+            <TableContainer hidden={false}>
+              <Table variant="simple">
+                <Thead bg="#F9FAFB">
+                  <Tr>
+                    <Th>Name</Th>
+                    <Th>Email</Th>
+                    <Th>Wallet</Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {Array.isArray(eventAttendees) &&
+                    eventAttendees.map((attendee) => (
+                      <Tr key={attendee?.decrypted.user_address}>
+                        <Td>{attendee?.decrypted.name}</Td>
+                        <Td>{attendee?.decrypted.email}</Td>
+                        <Td>{attendee?.decrypted.user_address}</Td>
+                      </Tr>
+                    ))}
+                </Tbody>
+              </Table>
+            </TableContainer>
+          </Stack>
         </Stack>
       </Container>
-
-      {/* <TableContainer hidden={false}>
-        <Table variant="simple">
-          <Thead>
-            <Tr>
-              <Th>Name</Th>
-              <Th>Email</Th>
-              <Th>Address</Th>
-              <Th>Is Going</Th>
-            </Tr>
-          </Thead>
-          <Tbody>
-            {Array.isArray(eventAttendees) &&
-              eventAttendees.map((attendee) => (
-                <Tr key={attendee?.decrypted.user_address}>
-                  <Td>{attendee?.decrypted.name}</Td>
-                  <Td>{attendee?.decrypted.email}</Td>
-                  <Td>{attendee?.decrypted.user_address}</Td>
-                  <Td>{String(attendee?.decrypted.isGoing)}</Td>
-                </Tr>
-              ))}
-          </Tbody>
-        </Table>
-      </TableContainer> */}
     </Layout>
   )
 }
