8212625964044a0b6eeaae5569499b92c8aedc42
drumfeet <drumfeet@gmail.com>
2023-05-18 14:26:31 +0800

    wip

--- decentraevent/components/UploadPhotoEvent.js
+++ decentraevent/components/UploadPhotoEvent.js
@@ -1,18 +1,53 @@
-import { Box, Stack, Text } from "@chakra-ui/react"
+import { Box, Text } from "@chakra-ui/react"
+import { useCallback } from "react"
+import { useDropzone } from "react-dropzone"
 import { GoCloudUpload } from "react-icons/go"
+import { toast } from "react-toastify"
 
 export default function UploadPhotoEvent() {
+  const onDrop = useCallback((acceptedFiles) => {
+    console.log("onDrop() acceptedFiles", acceptedFiles)
+  }, [])
+
+  const onDropRejected = useCallback((rejectedFiles) => {
+    console.log("onDropRejected() rejectedFiles", rejectedFiles)
+    toast(`${rejectedFiles[0].file.name} image file is not accepted`)
+  }, [])
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop,
+    onDropRejected,
+    maxFiles: 1,
+    accept: {
+      "image/png": [".png"],
+      "image/jpeg": [".jpg", ".jpeg"],
+      "image/svg+xml": [".svg"],
+    },
+  })
+
   return (
-    <Box borderWidth="1px" p="8px" borderColor="#98A2B3">
-      <Stack align="center" justify="center" textAlign="center">
-        <GoCloudUpload size="28px" />
-        <Text fontSize="16px" fontWeight="500">
-          This feature is not yet available
-        </Text>
-        <Text fontSize="14px">Upload photo for event</Text>
-        <Text fontSize="12px">Click to upload or drag and drop</Text>
-        <Text fontSize="10px">SVG, PNG, JPG or GIF (max. 800x400px)</Text>
-      </Stack>
-    </Box>
+    <>
+      <div {...getRootProps()}>
+        <input {...getInputProps()} />
+        <Box
+          borderWidth="1px"
+          p="8px"
+          borderColor="#98A2B3"
+          _hover={{
+            boxShadow: "4px 4px 0px black",
+            bg: "white",
+          }}
+        >
+          <GoCloudUpload size="28px" />
+          {isDragActive ? (
+            <Text fontSize="14px">Drop image file to upload</Text>
+          ) : (
+            <Text fontSize="14px">Upload photo for event</Text>
+          )}
+          <Text fontSize="12px">Click to upload or drag and drop</Text>
+          <Text fontSize="10px">SVG, PNG, JPG (max. 800x400px)</Text>
+        </Box>
+      </div>
+    </>
   )
 }
