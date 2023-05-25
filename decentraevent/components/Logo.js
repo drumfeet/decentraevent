2a4fe7faf0b294a90fbc3fefdbd99c5f2b53a054
drumfeet <drumfeet@gmail.com>
2023-05-25 11:33:09 +0800

    wip

--- decentraevent/components/Logo.js
+++ decentraevent/components/Logo.js
@@ -33,8 +33,8 @@ const LogoSVG = () => (
     <path
       d="M13.9785 4.56982C13.9785 4.56982 19.8114 15.3333 26.2134 15.3333C32.6154 15.3333 38.0215 4.56982 38.0215 4.56982"
       stroke="white"
-      stroke-width="2.23656"
-      stroke-linecap="round"
+      strokeWidth="2.23656"
+      strokeLinecap="round"
     />
     <rect
       y="25.1184"
@@ -54,20 +54,20 @@ const LogoSVG = () => (
     <path
       d="M38.021 43.4305C38.021 43.4305 32.1881 32.6671 25.7861 32.6671C19.3841 32.6671 13.978 43.4305 13.978 43.4305"
       stroke="white"
-      stroke-width="2.23656"
-      stroke-linecap="round"
+      strokeWidth="2.23656"
+      strokeLinecap="round"
     />
     <path
       d="M8.66699 36.0214C8.66699 36.0214 19.4304 30.1884 19.4304 23.7865C19.4304 17.3845 8.66699 11.9784 8.66699 11.9784"
       stroke="white"
-      stroke-width="2.23656"
-      stroke-linecap="round"
+      strokeWidth="2.23656"
+      strokeLinecap="round"
     />
     <path
       d="M43.3335 11.9785C43.3335 11.9785 32.5701 17.8114 32.5701 24.2134C32.5701 30.6154 43.3335 36.0215 43.3335 36.0215"
       stroke="white"
-      stroke-width="2.23656"
-      stroke-linecap="round"
+      strokeWidth="2.23656"
+      strokeLinecap="round"
     />
   </svg>
 )
